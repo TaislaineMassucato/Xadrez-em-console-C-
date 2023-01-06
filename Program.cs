280308f447dcddbf7b9dd7993c8f814ff7545ecc
Taislaine Massucato <massucato2@outlook.com>
2023-01-06 14:57:15 -0300

    Criando classe Posição

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using tabuleiro;
+using tabuleiro;
+
+namespace xadrex_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+                P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.WriteLine();
+        }
+    }   
+}
