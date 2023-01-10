22b55c269cab92615fe8018a4bbdb77a9ce9e54d
Taislaine Massucato <massucato2@outlook.com>
2023-01-10 12:22:03 -0300

    Melhorando a implementação do método imprimirTabuleiro

--- Tela.cs
+++ Tela.cs
@@ -1,6 +1,6 @@
 ﻿using tabuleiro;
 
-namespace Xadrex_Console
+namespace Xadrez_Console
 {
     internal class Tela
     {
@@ -8,6 +8,8 @@ namespace Xadrex_Console
         {
             for (int i=0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -16,10 +18,27 @@ namespace Xadrex_Console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
