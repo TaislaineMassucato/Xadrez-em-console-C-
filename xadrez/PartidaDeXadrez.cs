4c6793cad4c32a342c421f74f57c7bc13dee4681
Taislaine Massucato <massucato2@outlook.com>
2023-01-12 11:05:30 -0300

    Jogada especial Promoção

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -142,7 +142,20 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce não pode se colocar em em xeque");
             }
+            Peca p = tab.peca(destino);
 
+            //#jogadaespecial Promoção
+            if(p is Peao)
+            {
+                if(p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -160,8 +173,6 @@ namespace xadrez
                 turno = turno + 1;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //#JOGADAESPECIAL ENPASSANT
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
